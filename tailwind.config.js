ca21e807bff04b5fd93c3577327fa0c31f59ac71
Daniel Berg <mail@roosta.sh>
2021-03-26 12:58:09 +0100

    Extend inset, move theme extensions to 'extend'

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,20 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'title4': ['5.5rem', 0.8],
+        'title3': ['4.4rem', 0.8],
+        'title2': ['3.1rem', 0.8],
+        'title1': ['1.8rem', 0.8]
+      },
+      screens: {
+        '3xl': '1800px'
+      },
+      inset: {
+        '-6px': '-6px'
+      }
+    },
     backgroundColor: theme => ({
       ...theme('colors'),
       'primary': '#434343',
@@ -16,17 +29,6 @@ module.exports = {
       'display': ['Raleway Bold', ...defaultTheme.fontFamily.sans],
       'menu': ['Raleway Regular', ...defaultTheme.fontFamily.sans],
       'body': ['Open Sans', ...defaultTheme.fontFamily.sans],
-    },
-    fontSize: {
-      ...defaultTheme.fontSize,
-      'title4': ['5.5rem', 0.8],
-      'title3': ['4.4rem', 0.8],
-      'title2': ['3.1rem', 0.8],
-      'title1': ['1.8rem', 0.8]
-    },
-    screens: {
-      ...defaultTheme.screens,
-      '3xl': '1800px'
     },
   },
   variants: {
