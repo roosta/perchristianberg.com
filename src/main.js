fd9d2257af58ab0fc1fc05ece98351cc782bb159
Daniel Berg <mail@roosta.sh>
2021-03-31 17:46:05 +0200

    Add onClick handler for menu button

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import './style.css'
 import './bg1.jpg'
 
+function onMenuClick(e) {
+  console.log("hello click");
+
+}
+
 function main() {
+  const el = document.getElementById('menu-button');
+  el.addEventListener('click', onMenuClick);
 }
 
 main();
