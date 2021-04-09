e6b6a4e516e644c32ad6ce92f27dc6f8dbbfcae6
Daniel Berg <mail@roosta.sh>
2021-04-09 17:58:18 +0200

    Serve on lan

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,6 +6,7 @@ module.exports = merge(common, {
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist',
+    host: '0.0.0.0',
     open: {
       app: ['google-chrome-unstable', '--force-device-scale-factor=1']
     },
