2d2d612d7b3252bb1d7f674218c285a8eaa60e64
Daniel Berg <mail@roosta.sh>
2021-04-09 17:19:29 +0200

    Animate slide-in panel

--- src/main.js
+++ src/main.js
@@ -1,14 +1,35 @@
-import './style.css'
-import './bg1.jpg'
+import "./style.css"
+import "./bg1.jpg"
 
-function onMenuClick(e) {
-  console.log("hello click");
+const elements = {
+  overlay:   document.getElementById("slide-overlay"),
+  panel:     document.getElementById("slide-panel"),
+  container: document.getElementById("slide-container"),
+  openBtn:   document.getElementById("slide-menu-btn"),
+  closeBtn:  document.getElementById("slide-close-btn"),
+};
+
+function onMenuOpen(e, state) {
+  elements.container.classList.toggle("invisible");
+  elements.overlay.classList.replace("opacity-0", "opacity-100");
+  elements.panel.classList.replace("translate-x-full", "translate-x-0");
+  elements.closeBtn.classList.replace("opacity-0", "opacity-100");
 
 }
 
+function onMenuClose(e) {
+  elements.overlay.classList.replace("opacity-100", "opacity-0");
+  elements.panel.classList.replace("translate-x-0", "translate-x-full");
+  elements.closeBtn.classList.replace("opacity-100", "opacity-0");
+  elements.overlay.addEventListener("transitionend", function f() {
+    elements.container.classList.toggle("invisible");
+    this.removeEventListener("transitionend", f);
+  })
+}
+
 function main() {
-  const el = document.getElementById('menu-button');
-  el.addEventListener('click', onMenuClick);
+  elements.closeBtn.addEventListener("click", onMenuClose);
+  elements.openBtn.addEventListener("click", onMenuOpen);
 }
 
 main();
