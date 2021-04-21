baabd7e6c06efc3821c43bf5a46897712279134d
Daniel Berg <mail@roosta.sh>
2021-04-21 20:15:05 +0200

    Fix ngrok serve issues (invalid host-header)

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,6 +7,7 @@ module.exports = merge(common, {
   devServer: {
     contentBase: './dist',
     host: '0.0.0.0',
+    disableHostCheck: true,
     open: {
       app: ['google-chrome-unstable', '--force-device-scale-factor=1']
     },
