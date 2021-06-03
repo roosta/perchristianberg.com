df07220144fe797a20da15d96ab630890c007118
Daniel Berg <mail@roosta.sh>
2021-06-03 16:55:12 +0200

    Enable clean

--- webpack.common.js
+++ webpack.common.js
@@ -12,7 +12,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
-    // clean: true,
+    clean: true,
   },
   optimization: {
     moduleIds: 'deterministic',
