9f855fa4038a98a962b916239236f4c8fa488f60
Daniel Berg <mail@roosta.sh>
2021-08-23 15:45:43 +0200

    Update deps

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,14 +1,20 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist',
+    static: {
+      directory: path.resolve(__dirname, "static")
+    },
     host: '0.0.0.0',
     open: {
-      app: ['google-chrome-unstable', '--force-device-scale-factor=1']
+      app: {
+        name: "google-chrome-unstable",
+        arguments: ["--force-device-scale-factor=1"]
+      }
     },
   },
 });
