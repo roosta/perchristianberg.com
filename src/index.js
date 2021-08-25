59345c916d39973c4359bc02b408ab45038a11f7
Daniel Berg <mail@roosta.sh>
2021-08-25 19:09:03 +0200

    Reduce main margin down to 32

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import images from "../images.json";
 
 import { attachListeners, togglePips } from "./shared.js";
 
-const latestReleaseImg = document.querySelectorAll(".latest-release-img");
 const pips = document.querySelectorAll(".pip");
 const carouselAnnotation = document.getElementById("carousel-annotation");
 
