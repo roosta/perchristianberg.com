229abd06fc679b8f5518a6fbd4702968b260e090
Daniel Berg <mail@roosta.sh>
2021-08-25 18:02:44 +0200

    Revert webpack-dev-server update
    Issue where live reloading wouldn't work, despite having a pretty
    bare-bones config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,20 +1,27 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const path = require('path');
+// const path = require('path');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    static: {
-      directory: path.resolve(__dirname, "dist")
-    },
+    contentBase: './dist',
     host: '0.0.0.0',
     open: {
-      app: {
-        name: "google-chrome-unstable",
-        arguments: ["--force-device-scale-factor=1"]
-      }
+      app: ['google-chrome-unstable', '--force-device-scale-factor=1']
     },
   },
 });
+// devServer: {
+//     static: {
+//       directory: path.resolve(__dirname, "dist")
+//     },
+//     host: '0.0.0.0',
+//     open: {
+//       app: {
+//         name: "google-chrome-unstable",
+//         arguments: ["--force-device-scale-factor=1"]
+//       }
+//     },
+//   }
