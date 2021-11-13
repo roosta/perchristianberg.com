aac984c96c654a34ba44cd94680f9bb8f00960fa
Daniel Berg <mail@roosta.sh>
2021-11-13 13:25:27 +0100

    Upgrade all deps, fix config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,27 +1,21 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-// const path = require('path');
+const path = require('path');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist',
+    watchFiles: 'src/**/*',
+    static: {
+      directory: path.resolve(__dirname, "dist")
+    },
     host: '0.0.0.0',
     open: {
-      app: ['google-chrome-unstable', '--force-device-scale-factor=1']
+      app: {
+        name: "google-chrome-unstable",
+        arguments: ["--force-device-scale-factor=1"]
+      }
     },
-  },
+  }
 });
-// devServer: {
-//     static: {
-//       directory: path.resolve(__dirname, "dist")
-//     },
-//     host: '0.0.0.0',
-//     open: {
-//       app: {
-//         name: "google-chrome-unstable",
-//         arguments: ["--force-device-scale-factor=1"]
-//       }
-//     },
-//   }
